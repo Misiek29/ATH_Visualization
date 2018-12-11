1c446d8380f934c3ada623e4414f4b3746dca6e8
Mateusz Stępień <m.stepien29@gmail.com>
2018-12-11 10:29:49 +0100

    Add unity project

---
+++ ATHVisualisation/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float movementSpeed;
+    private CharacterController charController;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        PlayerMovement();
+    }
+    private void PlayerMovement()
+    {
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
+        Vector3 forwardMovement = Camera.main.transform.forward * vertInput;
+        Vector3 rightMovement = Camera.main.transform.right * horizInput;
+        charController.SimpleMove(forwardMovement + rightMovement);
+        //JumpInput();
+    }
+  
+}
